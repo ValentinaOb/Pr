600c5e2c3c7f247b8ff3250799506d2d3c04b38c
User <User@DESKTOP-KOBUB7D>
2023-05-30 13:17:15 +0300

    Pr

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -37,49 +37,277 @@ int main1()
     return 0;
 }
 
-void Print(const list<float>& lst) {
-    for (auto i = lst.begin(); i != lst.end(); i++) {
-        cout << *i << " ";
+
+template <class T>
+struct Element
+{
+    T data; 
+    Element<T>* next;
+    Element<T>* prev;
+};
+template <class T>
+class List
+{
+private:
+    Element<T>* begin; 
+    Element<T>* end; 
+    int count; 
+
+    Element<T>* Move(int index);
+
+    void Copy(const List<T>& obj);
+
+public:
+    List();
+
+    List(const List& obj);
+
+    List<T>& operator=(const List& obj);
+
+    ~List();
+
+    void SetElement(T _data, int index);
+
+    void AddEnd(T _data);
+
+    void AddBegin(T _data);
+
+    void Insert(T _data, int index);
+
+    void Del(int index);
+
+    void Clear();
+
+    void Reverse();
+
+    void Print(string msg);
+};
+
+template <class T>
+Element<T>* List<T>::Move(int index)
+{
+    Element<T>* t = begin;
+
+    for (int i = 0; i < index; i++)
+        t = t->next;
+
+    return t;
+}
+template <class T>
+void List<T>::Copy(const List<T>& obj)
+{
+    Clear();
+
+    Element<T>* t = obj.begin;
+
+    while (t != nullptr)
+    {
+        AddEnd(t->data);
+        t = t->next;
     }
 }
-int main2() {
-    list<float>numb = { 1.2,7,8,6,5,2,9,10,3,1.1 };
+template <class T>
+List<T>::List()
+{
+    begin = end = nullptr;
+    count = 0;
+}
+template <class T>
+List<T>::List(const List& obj)
+{
+    Copy(obj);
+}
+template <class T>
+List<T>& List<T>::operator=(const List& obj)
+{
+    Copy(obj);
+    return *this;
+}
+template <class T>
+List<T>::~List()
+{
+    Clear();
+}
+template <class T>
+void List<T>::AddEnd(T _data)
+{
+    try
+    {
+        Element<T>* t = new Element<T>;
+        t->next = nullptr; 
+        t->prev = end; 
+        t->data = _data; 
 
-    cout << "List: ";
-    Print(numb);
+        if (end != nullptr)
+            end->next = t;
 
-    cout << "\nSort: ";
-    numb.sort();
-    Print(numb);
-    list<float>numb1 = numb;
+        if (count == 0)
+        {
+            begin = end = t;
+        }
+        else
+        {
+            end = t;
+        }
 
-    cout << "\nNew list: ";
+        count++;
+    }
+    catch (bad_alloc e)
+    {
+        cout << e.what() << endl;
+    }
+}
+template <class T>
+void List<T>::AddBegin(T _data)
+{
+    try
+    {
+        Element<T>* t = new Element<T>;
+        t->data = _data; 
+        t->prev = nullptr; 
 
-    numb.merge(numb1);
+        t->next = begin;
 
-    int n = 0;
-    for (auto i = numb.begin(); i != numb.end(); i++) {
-        if (n == 5) { cout << "\n"; n = 0; }
-        n += 1;
-        cout << *i << " ";
+        if (count > 0)
+        {
+            begin->prev = t;
+            begin = t;
+        }
+        else
+        {
+            begin = end = t;
+        }
+
+        count++;
+    }
+    catch (bad_alloc e)
+    {
+        cout << e.what() << endl;
+    }
+}
+template <class T>
+void List<T>::Insert(T _data, int index)
+{
+
+    if (index == count)
+    {
+        AddEnd(_data);
+        return;
     }
 
-    cout << "\n\nOR\n";
-    list<float>k = { 1.2,7,8,6,5,2,9,10,3,1.1 };
-    cout << "\nRes: ";
-    k.sort();
-    list<float>k1 = k;
-    k.reverse();
-    for (auto i = k.begin(); i != k.end(); i++) {
-        if (n == 5) { cout << "\n"; n = 0; }
-        n += 1;
-        cout << *i << " ";
+    if (index == 0)
+    {
+        AddBegin(_data);
+        return;
     }
-    for (auto i = k1.begin(); i != k1.end(); i++) {
-        if (n == 5) { cout << "\n"; n = 0; }
-        n += 1;
-        cout << *i << " ";
+
+    try
+    {
+        Element<T>* itemPrev = Move(index - 1);
+
+        Element<T>* item = Move(index);
+
+        Element<T>* t = new Element<T>;
+        t->data = _data;
+        t->next = item;
+        t->prev = itemPrev;
+        itemPrev->next = t;
+        item->prev = t;
+
+        count++;
     }
+    catch (bad_alloc e)
+    {
+        cout << e.what() << endl;
+    }
+}
+template <class T>
+void List<T>::Del(int index)
+{
+    if (count == 0) return;
+
+    Element<T>* item = Move(index);
+
+    Element<T>* itemPrev = item->prev;
+
+    Element<T>* itemNext = item->next;
+
+    if ((count > 1) && (itemPrev != nullptr))
+        itemPrev->next = itemNext; 
+
+    if ((itemNext != nullptr) && (count > 1))
+        itemNext->prev = itemPrev;
+
+    if (index == 0)
+        begin = itemNext;
+
+    if (index == count - 1)
+        end = itemPrev;
+
+    delete item;
+
+    count--;
+}
+template <class T>
+void List<T>::Clear()
+{
+    int n = count; 
+    for (int i = 0; i < n; i++)
+        Del(0);
+}
+template <class T>
+void List<T>::Reverse()
+{
+    List<T> L;
+    Element<T>* t = begin;
+
+    while (t != nullptr)
+    {
+        L.AddBegin(t->data);
+        t = t->next;
+    }
+    *this = L;
+}
+template <class T>
+void List<T>::Print(string msg)
+{
+    cout << msg << " => ";
+
+    Element<T>* t = begin;
+    for (int i = 0; i < count; i++)
+    {
+        cout << t->data << " ";
+        t = t->next;
+    }
+    cout << endl;
+}
+
+int main2()
+{
+    List<double> L;
+
+    cout << "Input elements: ";
+    int i = 0, i1 = 0, i2=0, i3 = 0, i4 = 0;
+    cin >> i >> i1 >> i2 >> i3 >> i4;
+    L.AddBegin(i);
+    L.AddEnd(i1);
+    L.AddEnd(i2);
+    L.AddEnd(i3);
+    L.AddEnd(i4);
+    L.Print("L: ");
+
+    L.Reverse();
+    L.Print("L: ");
+
+    List<double> L1 = L;
+    L1.Print("L1: ");
+
+    L1.AddEnd(i);
+    L1.AddEnd(i1);
+    L1.AddEnd(i2);
+    L1.AddEnd(i3);
+    L1.AddEnd(i4);
+    L1.Print("L1: ");
 
     return 0;
 }
